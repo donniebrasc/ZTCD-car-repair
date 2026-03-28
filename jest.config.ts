6251ba13e4f9bbdaf35721a7941f6e6495718846
turnhoss187 <turnhoss187@gmail.com>
2026-03-28 21:33:25 +0000

    fixes

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,15 @@
 
 export default {
-  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '^@/(.*)$': '<rootDir>/src/$1',
-    '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        useESM: true,
-      },
-    ],
+    '^.+\.(ts|tsx|js|jsx)$': 'babel-jest',
   },
+  transformIgnorePatterns: [
+    '/node_modules/(?!p-retry|p-queue|react-markdown|@google/genai)/',
+  ],
 };
